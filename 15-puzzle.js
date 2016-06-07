ef6562fd9c76d785f38072afa0732f01af2df589
Ievgeniia Miroshko <i.miroshko@kiev.astoundcommerce.com>
2016-06-07 15:53:06 +0300

    ''

--- 15-puzzle.js
+++ 15-puzzle.js
@@ -1,38 +1,34 @@
-var puzzles=document.getElementById("puzzles");
+var puzzles=$("#puzzles");
 var puzzleArr=[];
 var counter=1;
-$("#button").click(function(){
-
+$("#button").on('click', function(){
 	createPuzzle(4);
 });
 
 function createPuzzle(puzzles){
 	var table=$('<table>');
-	$("body").append(table);
-	
-	for (var i = 0; i <puzzles; i++){
-		var tr=$('<tr>');
-		$("table").append(tr);
-		
+	$("body").append(table);	
+	for (var i = 0; i <puzzles; i++){		
+		var tr=$('<tr data-index='+(i+1)+'>');
+		$("table").append(tr);		
 		for (var j=0 ; j< puzzles; j++){
-			var td=$('<td>');
-			
+			var td=$('<td data-index='+(j+1)+'>');			
 			$("tr:last").append(td);
 			puzzleArr.push($("td:last"));
-
 		}
 	}
 	var freePlace=$("td:last");
 	for (var i = 0; i < 15; i++) {
 		$(puzzleArr[i]).html(i+1);
-	};
-	
-	$("td").click(function(){
-		alert(puzzleArr.indexOf($(this).html()))
-  		$(freePlace).html($(this).html());
-  		$(this).html("");
-  		freePlace=this; 	
-  			
-  	})
-	
+	};	
+	$("td").on('click', function(){
+		if ($(this).html()!==""){
+			var tdIndex = $(this).attr("data-index");
+			var trIndex = $(this).parent().attr("data-index");
+			alert(tdIndex+trIndex);
+			$(freePlace).html($(this).html());
+			$(this).html("");
+			freePlace=this; 
+		}			  			
+  	})	
 }
